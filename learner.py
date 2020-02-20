15cbbb5f0e8765325f18a95565efedd14fea2662
Shinnosuke Usami <susami@andrew.cmu.edu>
2020-02-20 14:51:01 -0500

    change affine number to match openai baseline

--- learner.py
+++ learner.py
@@ -18,8 +18,8 @@ class QCNN(nn.Module):
         self.conv1 = nn.Conv2d(state_dim, 32, kernel_size=8, stride=4)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
-        self.fc4 = nn.Linear(7 * 7 * 64, 512)
-        self.fc5 = nn.Linear(512, n_actions)
+        self.fc4 = nn.Linear(7 * 7 * 64, 256)
+        self.fc5 = nn.Linear(256, n_actions)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
